425aacaabbb300c1ef6814d22c362135d3f537a1
Raphael Geissler <raphael.geissler@gmail.com>
2017-03-20 13:14:48 +0100

    Fixed run configuration.

--- src/de.ragedev.example.jaxrs.service/src/de/ragedev/example/webservice/ExampleService.java
+++ src/de.ragedev.example.jaxrs.service/src/de/ragedev/example/webservice/ExampleService.java
@@ -13,14 +13,10 @@ import org.osgi.service.component.annotations.Component;
 @Path("/hello")
 public class ExampleService {
 
-	public ExampleService() {
-		// TODO Auto-generated constructor stub
-	}
-	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public String hello(String value) {
-		System.out.println("hello " + value);
-		return "hello " + value;
+		System.out.println("--> " + value);
+		return "Hello " + value;
 	}
 }
